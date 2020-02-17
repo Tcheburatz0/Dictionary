3f90ddb20bb3cb397009883479fba4177f1c3c77
Tcheburatz0 <cniko446@gmail.com>
2020-02-17 11:41:38 +0300

    Dictionary

---
+++ Source/concurrent_dictionary.hpp
@@ -0,0 +1,70 @@
+﻿/*
+Необходимо реализовать класс контейнера, реализующий интерфейс Dictionary и способный выбросить
+исключение, реализующее интерфейс NotFoundException.
+При разработке допускается использовать STL. Кроме этого, внутренняя реализация ничем не ограничена.
+
+template<class TKey, class TValue>
+class Dictionary
+{
+public:
+	virtual ~Dictionary() = default;
+
+	virtual const TValue& Get(const TKey& key) const = 0;
+	virtual void Set(const TKey& key, const TValue& value) = 0;
+	virtual bool IsSet(const TKey& key) const = 0;
+};
+
+template<class TKey>
+class NotFoundException : public std::exception
+{
+public:
+	virtual const TKey& GetKey() const noexcept = 0;
+};
+*/
+
+#pragma once
+
+#include <cstddef>
+#include <exception>
+#include <shared_mutex>
+#include <unordered_map>
+
+#include "dictionary.hpp"
+
+
+namespace vv
+{
+
+template <class TKey, class TValue>
+class ConcurrentDictionary final : public Dictionary<TKey, TValue>
+{
+public:
+	ConcurrentDictionary() = default;
+	
+	ConcurrentDictionary(const ConcurrentDictionary&) = delete;
+	
+	ConcurrentDictionary(ConcurrentDictionary&&) = delete;
+	
+	ConcurrentDictionary& operator=(const ConcurrentDictionary&) = delete;
+	
+	ConcurrentDictionary& operator=(ConcurrentDictionary&&) = delete;
+	
+	~ConcurrentDictionary() override = default;
+
+	const TValue& Get(const TKey& key) const override;
+
+	void Set(const TKey& key, const TValue& value) override;
+
+	bool IsSet(const TKey& key) const override;
+
+	std::size_t Size() const noexcept;
+
+private:
+	std::unordered_map<TKey, TValue> _data;
+
+	mutable std::shared_mutex _mutex;
+};
+
+} // namespace vv
+
+#include "concurrent_dictionary.inl"
---
+++ Source/concurrent_dictionary.inl
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <iterator>
+#include <mutex>
+#include <shared_mutex>
+
+
+namespace vv
+{
+
+template <class TKey, class TValue>
+const TValue& ConcurrentDictionary<TKey, TValue>::Get(const TKey& key) const
+{
+    std::shared_lock lock(_mutex);
+
+    const auto search = _data.find(key);
+    if (search == std::end(_data))
+    {
+        throw KeyNotFoundException(key);
+    }
+    return search->second;
+}
+
+template <class TKey, class TValue>
+void ConcurrentDictionary<TKey, TValue>::Set(const TKey& key, const TValue& value)
+{
+    std::unique_lock lock(_mutex);
+
+    const auto search = _data.find(key);
+    if (search == std::end(_data))
+    {
+        _data.emplace(key, value);
+    }
+    else
+    {
+        search->second = value;
+    }
+}
+
+template <class TKey, class TValue>
+bool ConcurrentDictionary<TKey, TValue>::IsSet(const TKey& key) const
+{
+    std::shared_lock lock(_mutex);
+
+    const auto search = _data.find(key);
+    return search != std::end(_data);
+}
+
+template <class TKey, class TValue>
+std::size_t ConcurrentDictionary<TKey, TValue>::Size() const noexcept
+{
+    std::shared_lock lock(_mutex);
+
+    return std::size(_data);
+}
+
+} // namespace vv
